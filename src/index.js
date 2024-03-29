954efcc097bf16ef99fde0636fa1db4305f2d17c
devictor8 <joaosalgado605@gmail.com>
2024-03-29 16:40:04 -0300

    add: cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/tasksRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3333;
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
